34d7a56d85b1b93beb9fedee141850e2dab5d0c5
Jyothika Karanam <jyothikakaranam991@gmail.com>
2026-08-18 18:14:37 +0530

    Time: 0 ms (100%), Space: 41.9 MB (70.96%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int fib(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        int prev0 = 0;
+        int prev1 = 1;
+        for (int i = 2; i <= n; i++) {
+            int next = prev1 + prev0;
+            prev0 = prev1;
+            prev1 = next;
+        }
+        return prev1;
+    }
+}
